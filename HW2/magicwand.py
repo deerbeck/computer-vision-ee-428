2aa6aa2f85d1a4619d999245265a4a46ef47b865
deerbeck <Johannes.Hi98@gmx.de>
2023-10-25 15:44:16 -0700

    finalizing hw2 and getting solution done

--- HW2/magicwand.py
+++ HW2/magicwand.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
 
+
 class MagicWand:
-    def __init__(self,calibration_path,R):
+    def __init__(self, calibration_path, R):
         """ Loads calibration from file and stores ball radius.
             Arguments:
                 calibration_path: path to calibration file
                 R: ball radius in cm
         """
-        self.focal, self.centerx, self.centery = np.loadtxt(calibration_path,delimiter=' ')
+        self.focal, self.centerx, self.centery = np.loadtxt(
+            calibration_path, delimiter=' ')
         self.R = R
 
-    def detect_ball(self,image):
+    def detect_ball(self, image):
         """ Detect one or more balls in image.
             Arguments:
                 image: RGB image in which to detect balls
@@ -21,22 +23,26 @@ class MagicWand:
         # Pre-Processing of the image to use the ball detection on it
         # Start with converting frame into gray_scale.
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        
+
         # Applying gaussian blur on it
         # Kernel size of 9x9 and sigma = 2
-        kernel_size = (9,9)
+        kernel_size = (9, 9)
         sigma = 2
-        image_gauss = cv2.GaussianBlur(image_gray,kernel_size,sigma)
+        image_gauss = cv2.GaussianBlur(image_gray, kernel_size, sigma)
 
         # Run HughCircles to detect circles
-        circles = cv2.HoughCircles(image_gauss, method=cv2.HOUGH_GRADIENT, dp=2, minDist = 10)
+        circles = cv2.HoughCircles(
+            image_gauss, method=cv2.HOUGH_GRADIENT, dp=2, minDist=10)
 
-        # rearrange list of circles to match desired return type
-        circles = list(map(tuple,circles[0, :]))
+        # rearrange list of circles to match desired return type if circles have been found
+        if circles is not None:
+            circles = list(map(tuple, circles[0, :]))
+            return circles
+        # return empty tuple when no circles have been detected
+        else:
+            return ()
 
-        return circles
-
-    def calculate_ball_position(self,x,y,r):
+    def calculate_ball_position(self, x, y, r):
         """ Calculate ball's (X,Y,Z) position in world coordinates
             Arguments:
                 x,y: 2D position of ball in image
@@ -45,53 +51,108 @@ class MagicWand:
                 X,Y,Z position of ball in world coordinates
         """
         # Applying projection equationws
+        # Start with calculating Z
+        Z = self.focal * (self.R / r)
 
-        pass
+        # Calculate X and Y with newly calculated Z
+        X = ((x - self.centerx) * Z) / (self.focal)
+        Y = ((y - self.centery) * Z) / (self.focal)
 
-    def draw_ball(self,image,x,y,r,Z):
+        return (X, Y, Z)
+
+    def draw_ball(self, image, x, y, r, Z):
         """ Draw circle on ball and write depth estimate  in center
             Arguments:
                 image: image on which to draw
                 x,y,r: 2D position and radius of ball
                 Z: estimated depth of ball
         """
-
-        cv2.circle( image, (int(x),int(y)), int(r), (0,0,255),2)
-        cv2.putText( image, str(int(Z)) + ' cm', (int(x),int(y)), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255))
+        cv2.circle(image, (int(x), int(y)), int(r), (0, 0, 255), 2)
+        cv2.putText(image, str(int(Z)) + ' cm', (int(x), int(y)),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
     
-    def project(self,X,Y,Z):
+    def project(self, X, Y, Z):
         """ Pinhole projection.
             Arguments:
                 X,Y,Z: 3D point
             Returns:    
                 (x,y) 2D location of projection in image
         """
-        pass
+        # calculate 2D coordinates using projection equations
+        x = ((self.focal * X) / Z) + self.centerx
+        y = ((self.focal * Y) / Z) + self.centery
 
-    def draw_line(self,image,X1,Y1,Z1,X2,Y2,Z2):
+        return (x, y)
+
+    def draw_line(self, image, X1, Y1, Z1, X2, Y2, Z2):
         """ Draw a 3D line
             Arguments:
                 image: image on which to draw
                 X1,Y1,Z1: 3D position of first line endpoint
                 X2,Y2,Z2: 3D position of second line endpoint
         """
-        pass
+        # first convert 3D Coordinates into 2D Coordinates
+        x1, y1 = self.project(X1, Y1, Z1)
+        x2, y2 = self.project(X2, Y2, Z2)
 
-    def draw_bounding_cube(self,image,X,Y,Z):
+        # then draw the line (thickness = 1)
+        cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+
+    def draw_bounding_cube(self, image, X, Y, Z):
         """ Draw bounding cube around 3D point, with radius R
             Arguments:
                 image: image on which to draw
                 X,Y,Z: 3D center point of cube
         """
-        pass
-    
-    def process_frame(self,image):
+        # calculate all corners of the cube (8 in total):
+        C0 = (X + self.R, Y + self.R, Z + self.R)
+        C1 = (X + self.R, Y + self.R, Z - self.R)
+        C2 = (X + self.R, Y - self.R, Z + self.R)
+        C3 = (X + self.R, Y - self.R, Z - self.R)
+        C4 = (X - self.R, Y + self.R, Z + self.R)
+        C5 = (X - self.R, Y + self.R, Z - self.R)
+        C6 = (X - self.R, Y - self.R, Z + self.R)
+        C7 = (X - self.R, Y - self.R, Z - self.R)
+
+        # draw the lines of the cube
+        self.draw_line(image, *C0, *C1)
+        self.draw_line(image, *C0, *C2)
+        self.draw_line(image, *C0, *C4)
+        self.draw_line(image, *C1, *C3)
+        self.draw_line(image, *C1, *C5)
+        self.draw_line(image, *C2, *C3)
+        self.draw_line(image, *C2, *C6)
+        self.draw_line(image, *C3, *C7)
+        self.draw_line(image, *C4, *C5)
+        self.draw_line(image, *C4, *C6)
+        self.draw_line(image, *C5, *C7)
+        self.draw_line(image, *C6, *C7)
+
+    def process_frame(self, image):
         """ Detect balls in frame, estimate 3D positions, and draw on image
             Arguments:
                 image: image to be processed
             Returns:
                 list of (X,Y,Z) 3D points of detected balls
         """
-        self.detect_ball(image)
-        pass
+        # empty array to be filled with detected balls
+        all_balls = []
+        # detect circles in image using detect_ball method
+        circles = self.detect_ball(image)
 
+        # if circles have been found
+        if len(circles):
+
+            # iterate through circles if multiple have been found
+            for circle in circles:
+                # calculate real world coordinated for the ball and draw it
+                X, Y, Z = self.calculate_ball_position(*circle)
+                self.draw_ball(image, *circle, Z)
+
+                # draw bounding cube using the draw bounding cube method
+                self.draw_bounding_cube(image, X, Y, Z)
+
+                # append 3D Coordinates to balls
+                all_balls.append((X, Y, Z))
+
+        return all_balls
--- HW2/tracker.py
+++ HW2/tracker.py
@@ -1,28 +1,85 @@
 import cv2
-
 from magicwand import *
-
 import argparse
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot as plt
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('video',help='path to input video file')
-parser.add_argument('--output',help='path to output video file (optional)')
-parser.add_argument('--calibration',default='iphone_calib.txt',help='path to calibration file')
-parser.add_argument('--ball_radius',type=float,default=3,help='radius of ball in cm')
+parser.add_argument('video', help='path to input video file')
+parser.add_argument('--output', help='path to output video file (optional)')
+parser.add_argument('--calibration', default='iphone_calib.txt',
+                    help='path to calibration file')
+parser.add_argument('--ball_radius', type=float,
+                    default=3, help='radius of ball in cm')
 args = parser.parse_args()
 
-wand = MagicWand(calibration_path=args.calibration,R=args.ball_radius)
+wand = MagicWand(calibration_path=args.calibration, R=args.ball_radius)
 
 cap = cv2.VideoCapture(args.video)
+
+single_balls = []
+distances = []
+
+to_im = 0
+double = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
-    
 
-    wand.process_frame(frame)
-     
-    cv2.imshow('frame',frame)
+    # draw circles, cube and get detected circles in the frame
+    all_balls = wand.process_frame(frame)
+
+    # capture single ball for 3D Plot
+    if len(all_balls) == 1:
+        single_balls.append(all_balls[0])
+
+    # calculate distance for wand
+    elif len(all_balls) == 2:
+        # get delta coordinates for 3D Distance Calculation
+        dX, dY, dZ = np.array(all_balls[0])-np.array(all_balls[1])
+        distance = np.sqrt(dX**2 + dY**2 + dZ**2)
+
+        # append to distances list to mean out later
+        distances.append(distance)
+        double += 1
+
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
+    to_im += 1
+
+print(f"Single Ball Ration : {len(single_balls)/to_im}")
+print(f"Double Ball Ration : {double/to_im}")
+
+# plotting 3D trajectory of Ball:
+single_circles = np.array(single_balls)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+# extract 3D points from list
+xdata = single_circles[:, 0]
+ydata = single_circles[:, 1]
+zdata = single_circles[:, 2]
+
+# plot Ball trajectory
+ax.plot3D(xdata, ydata, zdata, 'red')
+
+# Set labels for the axes
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# set initial view to the same as POV of camera
+ax.view_init(azim=-90, elev=-90)
+
+# show 3D Plot
+plt.show()
+
+# print out mean of distances
+print(f"Wand Distance: {np.mean(distances):.3f} cm")
+
+# mean distance for wand.mov: 36.902 cm, so pretty close to ground truth of 36 cm
